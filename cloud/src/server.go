5121bade1eed9b60b8aacc7492b0ef3ef09365c0
Carlos Arthur <carlos17.fsa15@gmail.com>
2025-03-20 16:26:01 -0300

    make listener server

--- cloud/src/server.go
+++ cloud/src/server.go
@@ -1,4 +1,4 @@
-package cloud
+package main
 
 import (
 	"encoding/json"
@@ -59,5 +59,22 @@ func HandleConnection(conn net.Conn) {
 }
 
 func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("Erro: ", err)
+		return
+	}
+	defer listener.Close()
 
+	fmt.Println("connected in port 8080")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Err acepte conection: ", err)
+			continue
+		}
+
+		go HandleConnection(conn)
+	}
 }
